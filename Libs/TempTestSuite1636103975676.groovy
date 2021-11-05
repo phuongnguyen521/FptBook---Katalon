c81c1e653a715f3782c030a3efa2c9ab9338741e
tiendvlp <tiendvlp@gmail.com>
2021-11-05 16:21:30 +0700

    edit docker file

--- Libs/TempTestSuite1636103975676.groovy
+++ Libs/TempTestSuite1636103975676.groovy
@@ -18,19 +18,19 @@ suiteProperties.put('id', 'Test Suites/FptBook_TestSuite')
 suiteProperties.put('name', 'FptBook_TestSuite')
 suiteProperties.put('description', '')
 suiteProperties.put('rerunTestFailImmediately', 'true')
-suiteProperties.put('retryCount', '3')
+suiteProperties.put('retryCount', '0')
  
 
-DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
-DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
-DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
 
 
 
-RunConfiguration.setExecutionSettingFile("/Users/dangminhtien/Desktop/katalon/SWT301-Katalon-FptBook/Reports/20211105_161326/FptBook_TestSuite/20211105_161326/execution.properties")
+RunConfiguration.setExecutionSettingFile("/tmp/project/Reports/20211105_091924/FptBook_TestSuite/20211105_091935/execution.properties")
 
 TestCaseMain.beforeStart()
 
-TestCaseMain.startTestSuite('Test Suites/FptBook_TestSuite', suiteProperties, new File("/Users/dangminhtien/Desktop/katalon/SWT301-Katalon-FptBook/Reports/20211105_161326/FptBook_TestSuite/20211105_161326/testCaseBinding"))
+TestCaseMain.startTestSuite('Test Suites/FptBook_TestSuite', suiteProperties, new File("/tmp/project/Reports/20211105_091924/FptBook_TestSuite/20211105_091935/testCaseBinding"))
